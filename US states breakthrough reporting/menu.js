5c87c33e3d562f4ca449d8c8fe917c8e20519be0
Michal Mart <michal.mart@gmail.com>
2022-01-18 14:35:56 -0500

    new menu items

--- US states breakthrough reporting/menu.js
+++ US states breakthrough reporting/menu.js
@@ -2,7 +2,13 @@ function onOpen() {
   var ui = SpreadsheetApp.getUi();
   ui.createMenu('Pandemic Tracking')
 
-  .addItem("--- Configuration", "empty")
-  .addItem('          Recreate Worksheet Settings', 'confirmReinitializeWorksheet')
+  .addItem('          Reinitialize Worksheet', 'confirmReinitializeWorksheet')
+  .addSeparator()
+  .addItem('          Clear Checkers, DCers and last check', 'confirmReinitializeChecks')
+  .addItem('          Reinitialize Notes', 'confirmReinitializeNotes')
+  .addItem('          Reinitialize Screenshot Status', 'confirmReinitializeScreenshotsStatus')
+  .addSeparator()
+  .addItem('          Create QA Sheet', 'createQASheet')
+
   .addToUi();
 }
